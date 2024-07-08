42d690102af16bbe7e09b4ca43c250b0d6d10bf1
Rufus <coperu932@gmail.com>
2024-07-08 09:55:15 +0100

    Initial practice and warmup

---
+++ script.js
@@ -0,0 +1,23 @@
+// let planets = ["Earth", "Jupiter", "Mars", "Venus"];
+// console.log(planets);
+// planets.push("Sun");
+// console.log(planets);
+// let pushed = planets.push();
+// console.log(pushed);
+// let popped = planets.pop();
+// console.log(planets);
+// console.log(popped);
+
+
+// const array1 = ["Hicapa", "SSG1", "AAP01"];
+// console.log(array1)
+// array1.push("Striker")
+// console.log(array1)
+// array1.push("Marker")
+// console.log(array1)
+// let fixedArray = array1
+// console.log(fixedArray.length)
+// console.log(fixedArray.length - 1)
+// let popped = fixedArray.pop();
+// console.log(popped)
+
